b10557ea67addac1bf7f99dbe4f32ad48ee1d7cd
SahilShah518 <sahilshah379@gmail.com>
2018-12-19 16:44:11 -0500

    yay it works

--- Clahe.py
+++ Clahe.py
@@ -3,53 +3,34 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 def images(img1,img2):
-	# while True:
-	cv.imshow('old', img1)
-	cv.imshow('new',img2)
-		# plt.show()
-		# k = cv.waitKey(30) & 0xff
-		# if k == 27:
-		# 	break
+	img1 = cv.resize(img1, (0,0), fx=0.5, fy=0.5)
+	img2 = cv.resize(img2, (0,0), fx=0.5, fy=0.5)
+	while True:
+		images = np.concatenate((img1, img2), axis=1)
+		cv.imshow('images', images)
+		k = cv.waitKey(30) & 0xff
+		if k == 27:
+			break
 
-def hist1(img1):
-	hist,bins = np.histogram(img1.flatten(),256,[0,256])
+def hist(img):
+	hist,bins = np.histogram(img.flatten(),256,[0,256])
 	cdf = hist.cumsum()
 	cdf_normalized = cdf * hist.max()/ cdf.max()
 	hist1 = plt.figure(1)
 	plt.plot(cdf_normalized, color = 'b')
-	plt.hist(img1.flatten(),256,[0,256], color = 'r')
+	plt.hist(img.flatten(),256,[0,256], color = 'r')
 	plt.xlim([0,256])
-	plt.legend(('cdf','histogram1'), loc = 'upper left')
+	plt.legend(('cdf','histogram'), loc = 'upper left')
 	hist1.show()
 
-def hist2(img2):
-	hist,bins = np.histogram(img2.flatten(),256,[0,256])
-	cdf = hist.cumsum()
-	cdf_normalized = cdf * hist.max()/ cdf.max()
-	hist2 = plt.figure(2)
-	plt.plot(cdf_normalized, color = 'b')
-	plt.hist(img2.flatten(),256,[0,256], color = 'r')
-	plt.xlim([0,256])
-	plt.legend(('cdf','histogram2'), loc = 'upper left')
-	hist2.show()
+def balance(img):
+	lab = cv.cvtColor(img, cv.COLOR_BGR2LAB)
+	lab[:,:,1] = lab[:,:,1]-((np.average(lab[:,:,1])-128)*(lab[:,:,0]/255.0)*1.1)
+	lab[:,:,2] = lab[:,:,2]-((np.average(lab[:,:,2])-128)*(lab[:,:,0]/255.0)*1.1)
+	new = cv.cvtColor(lab, cv.COLOR_LAB2BGR)
+	return new
 
 if __name__ == "__main__":
-	cap = cv.VideoCapture(0)
-	_,frame = cap.read()
-	img1 = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-	img1 = cv.imread('img2.jpg', 0)
-	# img2 = cv.equalizeHist(img1)
-	img2 = 
+	img1 = cv.imread('img1.jpg', 1)
+	img2 = balance(img1)
 	images(img1,img2)
-	hist1(img1)
-	hist2(img2)
-	input()
-
-# hist,bins = np.histogram(img.flatten(),256,[0,256])
-# cdf = hist.cumsum()
-# cdf_normalized = cdf * hist.max()/ cdf.max()
-# plt.plot(cdf_normalized, color = 'b')
-# plt.hist(img.flatten(),256,[0,256], color = 'r')
-# plt.xlim([0,256])
-# plt.legend(('cdf','histogram'), loc = 'upper left')
-# plt.show()
